67d96b7f7f4a98d3121999a3c29c4f5640a367ae
Erik Vaněk <erikvanek0@gmail.com>
2024-10-13 20:39:32 +0200

    added config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    // This file contains app settings which can be modified.
+
+    // Database login details:
+    return array(
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'schema' => 'zonmb',
+    );
