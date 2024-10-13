d753cea9e35ec552ceceb023418fa07f4fd55815
Erik Vaněk <erikvanek0@gmail.com>
2024-10-13 20:39:25 +0200

    added footer, header

---
+++ src/Views/Templates/footer.php
@@ -0,0 +1,6 @@
+
+        <footer>
+            <p>&copy; 2024 - Erik Vaněk</p>
+        </footer>
+    </body>
+</html>
---
+++ src/Views/Templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Oficiální stránka organizace neslyšících v Mladé Boleslavy">
+    <title><?php echo $title ?? 'ZONMB'; ?></title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>Option 1</li>
+            <li>Option 2</li>
+            <li>Option 3</li>
+        </ul>
+    </nav>
+    
